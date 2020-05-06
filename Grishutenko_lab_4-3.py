579e7269838744637f45883c74d18bf06dd66d9e
grishutenko <36900540+grishutenko@users.noreply.github.com>
2020-05-06 23:13:42 +0300

    rework lab_4-3

--- Grishutenko_lab_4-3.py
+++ Grishutenko_lab_4-3.py
@@ -13,13 +13,17 @@ except ImportError:
     print("Ошибка при попытке импартировать модуль")
 
 def test_t1():
-    assert dataPrint([{"Tony":123, "Dony":"rty", "Bob":[8,9,0]}]) == "Tony : 123 Dony : rty Bob : [8, 9, 0] "
-    assert dataPrint([{"Tony":123, "Dony":"rty", "Bob":[8,9,0]}]) != "Tony : 123 Dony : rty Bob : 8 9 0 "
+    assert dataPrint([{"Tony":123, "Dony":"rty", "Bob":[8,9,0]}]) == \
+        "Tony : 123 Dony : rty Bob : [8, 9, 0] "
+    assert dataPrint([{"Tony":123, "Dony":"rty", "Bob":[8,9,0]}]) != \
+        "Tony : 123 Dony : rty Bob : 8 9 0 "
 
 
-def test_t2(el):
+def test_t2(el): 
+    #в функцию должен передаваться словарь
     with pytest.raises(KeyError):
-        el.get("TEST_FILD") #намеренно заваленный тест
+        el.get("TEST_FILD") #намеренно заваленный тест,попытка получить 
+                            #значение по несуществующему ключу
 
 
 def dataPrint(data):
@@ -30,8 +34,9 @@ def dataPrint(data):
                 for item in element:
                     try:
                         print(item,' : ',element.get(item))
-                        strdata += str(item) +' : '+ str(element.get(item)) + " "
-                        test_t2(element)
+                        strdata += str(item) + ' : ' + \
+                            str(element.get(item)) + " "
+                        
                     except KeyError:
                         print('Неверное взятие по ключу')
 
@@ -48,10 +53,8 @@ try:
         except json.JSONDecodeError:
             print("не json")
 
-        #dataPrint(data) #если нужен вывод json
+        dataPrint(data) #если нужен вывод json
 except FileNotFoundError:
     print("не найден файл")
 except IOError:
     print("невозможно прочитать/записать. (возможна ошибка прав доступа)")
-
-
